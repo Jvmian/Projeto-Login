793d2555af4ceffc7212c4b65dbf5fafe1728939
João Victor Mian Valdomiro <joaovictormian@gmail.com>
2024-03-10 17:51:51 -0300

    Mais js

--- interacao/script.js
+++ interacao/script.js
@@ -4,6 +4,7 @@ login.addEventListener('mouseenter', entrar )
 login.addEventListener('mouseout', sair)
 senha.addEventListener('mouseenter', entrarr )
 senha.addEventListener('mouseout', sairr)
+
 function entrar(){
     login.style.backgroundColor = '#dfddbd'
 }
@@ -16,3 +17,30 @@ function entrarr(){
 function sairr(){
     senha.style.backgroundColor = '#016FB9'
 }
+function mostrarsenha(){
+   var senha = window.document.getElementById('isenha')
+   var mostrar = window.document.getElementById ('mostrar')
+   
+   if (senha.type === 'password'){
+    senha.setAttribute('type', 'text')
+    mostrar.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+   } else {
+    senha.setAttribute('type', 'password')
+    mostrar.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
+   }
+}
+
+
+
+// window.alert("funcionou")
+
+/*var senhaoculta = window.document.getElementById ('isenha')
+   if( senhaoculta.type == 'password'){
+    senhaoculta.type = 'Text'
+   } else {
+    senhaoculta.type = 'password'
+   }
+}
+function mudaricon(){
+    var imagem = window.document.getElementsByClassName ('material-symbols-outlined')
+    imagem.setAttribute('Visibility')*/
